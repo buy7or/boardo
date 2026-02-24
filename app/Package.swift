ca5ca1ea49410aef721b2ac0d7005bef36ddf68c
diego <buy7or@gmail.com>
2026-02-24 20:49:30 +0100

    inicio

---
+++ app/Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version: 6.0
+
+import PackageDescription
+
+let package = Package(
+    name: "app",
+    platforms: [
+        .iOS(.v17),
+        .macOS(.v14),
+    ],
+    products: [
+        // An xtool project should contain exactly one library product,
+        // representing the main app.
+        .library(
+            name: "app",
+            targets: ["app"]
+        ),
+    ],
+    targets: [
+        .target(
+            name: "app"
+        ),
+    ]
+)
